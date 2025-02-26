4ef5b95474dc9b4fe21a74383cb61ba56fac73d9
OtavioAL <otavioauglei@gmail.com>
2025-02-26 13:55:02 -0300

    feat: registering user routes

--- api-daily-diet/src/app.ts
+++ api-daily-diet/src/app.ts
@@ -1,6 +1,19 @@
 import fastify from "fastify";
 import cookie from "@fastify/cookie";
+import { env } from "./env";
+import { usersRoutes } from "./routes/users.routes";
 
 export const app = fastify();
 
 app.register(cookie);
+
+app.register(require("@fastify/jwt"), {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: "1d", // Token válido por 1 dia
+  },
+});
+
+app.register(usersRoutes, {
+  prefix: "/users",
+});
