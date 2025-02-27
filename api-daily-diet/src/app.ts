02af498a6328c2f0d9df4102cfe09b9801cc135d
OtavioAL <otavioauglei@gmail.com>
2025-02-26 21:31:55 -0300

    feat: recording meal routes

--- api-daily-diet/src/app.ts
+++ api-daily-diet/src/app.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 import cookie from "@fastify/cookie";
 import { env } from "./env";
 import { usersRoutes } from "./routes/users.routes";
+import { mealsRoutes } from "./routes/meals.routes";
 
 export const app = fastify();
 
@@ -16,4 +17,8 @@ app.register(require("@fastify/jwt"), {
 
 app.register(usersRoutes, {
   prefix: "/users",
+});
+
+app.register(mealsRoutes, {
+  prefix: "/meals",
 });
