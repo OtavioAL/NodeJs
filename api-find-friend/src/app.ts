ba62767a6278014cb749db74e117fadef71d16ae
OtavioAL <otavioauglei@gmail.com>
2025-03-19 10:57:39 -0300

    feat: registering new routes

--- api-find-friend/src/app.ts
+++ api-find-friend/src/app.ts
@@ -1,8 +1,32 @@
+/**
+ * 
+*    ### Regras da aplicação
+
+    - Deve ser possível cadastrar um pet
+    - Deve ser possível listar todos os pets disponíveis para adoção em uma cidade
+    - Deve ser possível filtrar pets por suas características
+    - Deve ser possível visualizar detalhes de um pet para adoção
+    - Deve ser possível se cadastrar como uma ORG
+    - Deve ser possível realizar login como uma ORG
+
+    ### Regras de negócio
+
+    - Para listar os pets, obrigatoriamente precisamos informar a cidade
+    - Uma ORG precisa ter um endereço e um número de WhatsApp
+    - Um pet deve estar ligado a uma ORG
+    - O usuário que quer adotar, entrará em contato com a ORG via WhatsApp
+    - Todos os filtros, além da cidade, são opcionais
+    - Para uma ORG acessar a aplicação como admin, ela precisa estar logada
+ * 
+ */
+
 import fastify from "fastify";
 import { ZodError } from "zod";
 import { env } from "./env";
 import fastifyJwt from "@fastify/jwt";
 import fastifyCookie from "@fastify/cookie";
+import { petsRoutes } from "./http/controllers/pets/routes";
+import { orgsRoutes } from "./http/controllers/orgs/routes";
 
 export const app = fastify();
 
@@ -18,6 +42,9 @@ app.register(fastifyJwt, {
 });
 
 app.register(fastifyCookie);
+
+app.register(petsRoutes);
+app.register(orgsRoutes);
 
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
